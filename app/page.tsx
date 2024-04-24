c06f610c8cf000e0a25bcb5f4cb64b698300ef24
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-04-24 14:25:18 +0900

    ページコンポーネントの更新とグローバルCSSの修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello World!</main>;
+  return (
+    <>
+      {/* ヘッダー */}
+      <div className="flex space-x-3">
+        <p>TailwindTemplates</p>
+        <p>Templates</p>
+        <p>Resources</p>
+        <p>Blog</p>
+        <p>About</p>
+      </div>
+
+      {/* メイン */}
+      <div>main</div>
+
+      {/* フッター */}
+      <div>footer</div>
+    </>
+  );
 }
